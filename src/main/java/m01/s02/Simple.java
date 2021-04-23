d0fca740f3bdd5993da7495c9d02171d7133284e
Marzia <Marzia@DESKTOP-VGEKPQ4.home>
2021-04-23 09:31:47 +0200

    prove

--- src/main/java/m01/s02/Simple.java
+++ src/main/java/m01/s02/Simple.java
@@ -12,7 +12,7 @@ import java.lang.Math; // not required
  */
 public class Simple {
     public static void main(String[] args) {
-        // a comment
+        // a commentss
         System.out.println(Math.PI);
     }
 }
